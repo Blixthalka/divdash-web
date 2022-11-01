86c79b9a84ce8c87e45e98019a4439f42812be70
Blixthalka <emil837@gmail.com>
2022-11-01 21:30:12 +0100

    remove nordnet

--- src/routes/Upload.jsx
+++ src/routes/Upload.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Card from '../components/Card';
 import avanza from '../images/avanza.png';
-import nordnet from '../images/nordnet.png';
 
 const Upload = () => {
     const navigate = useNavigate()
@@ -12,23 +11,23 @@ const Upload = () => {
             coming_soon: false,
             tag: "avanza"
         },
-        {
-            image: nordnet,
-            coming_soon: true,
-            tag: "nordnet"
-        }
+        // {
+        //     image: nordnet,
+        //     coming_soon: true,
+        //     tag: "nordnet"
+        // }
     ]
 
     return (
         <>
             <Card title="Choose Bank" >
-                <div className="grid grid-cols-2 gap-5 mt-2">
+                <div className="flex space-x-2 mt-2">
                     {banks.map((bank) => (
 
 
 
                         <button
-                            className={`border border-dashed border-slate-200 p-5 px-32 rounded flex flex-col group justify-center
+                            className={`border border-dashed border-slate-200 p-5 rounded flex flex-col group justify-center
                                 ${bank.coming_soon && 'cursor-default'}
                                 ${!bank.coming_soon && "cursor-pointer hover:border-secondary"}`}
                             onClick={(e) => {
@@ -38,7 +37,7 @@ const Upload = () => {
                             }}
                         >
                             {bank.coming_soon && <p className="place-self-center text-secondary">coming soon</p>}
-                            <img src={bank.image} className="p-2 backdrop-grayscale" disabled={bank.coming_soon} alt={bank.tag} />
+                            <img src={bank.image} className="p-2 backdrop-grayscale w-56" disabled={bank.coming_soon} alt={bank.tag} />
                         </button>
 
 
